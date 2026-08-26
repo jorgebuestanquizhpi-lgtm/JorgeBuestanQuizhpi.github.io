3b4119c238baf8120615189c3363af74e0266470
jorgebuestanquizhpi-lgtm <jorgebuestanquizhpi@gmail.com>
2026-08-26 18:13:17 -0500

    Agregar lista de galería de montaña

--- content.js
+++ content.js
@@ -16,6 +16,9 @@ window.PORTFOLIO_CONTENT = {
       description:
         "Selección de fotografía de paisaje, expediciones y experiencias en los Andes ecuatorianos.",
       image: "assets/images/fotografia-montana/koala-atardecer.webp",
+      gallery: [
+  "assets/images/fotografia-montana/koala-atardecer.webp",
+],
       size: "wide",
       position: "center 52%",
     },
